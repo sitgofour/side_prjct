c5b4104902d84274ab3f8af7dc6ffacf156c91e9
sitgofour <evansmithcodes@gmail.com>
2020-12-06 17:45:21 -0800

    notes, todo list

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [serverRes, setServerRes] = useState("");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setServerRes("page loaded");
+    }, 2000);
+  },[]);
+
+
   return (
     <div className="App">
       <h1>side_prjct</h1>
+      <p>{serverRes}</p>
     </div>
   );
 }
